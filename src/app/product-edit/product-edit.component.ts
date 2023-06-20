726ef97a06b2d0ce4a93dfe4a8d30afe455151f9
Pintu-Roy121 <roypintu121@gmail.com>
2023-06-21 02:30:43 +0600

    save data to localstorage

--- src/app/product-edit/product-edit.component.ts
+++ src/app/product-edit/product-edit.component.ts
@@ -14,6 +14,90 @@ export class ProductEditComponent implements OnInit {
   allList: any;
   remainProduct: any;
 
+  product = {
+    list: [
+      {
+        "id": 1,
+        "category": "Men's Sneaker",
+        "name": "ULTRABOOST 22 SHOES",
+        "seller": "Addidas",
+        "price": 420,
+        "stock": 20,
+        "ratings": 4,
+        "ratingsCount": 3725,
+        "img": "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/fbaf991a78bc4896a3e9ad7800abcec6_9366/Ultraboost_22_Shoes_Black_GZ0127_01_standard.jpg",
+        "shipping": 1,
+        "quantity": 0
+      },
+      {
+        "id": 2,
+        "category": "Men's Sneaker",
+        "name": "LUNAR NEW DNA SHOES",
+        "seller": "Addidas",
+        "price": 196,
+        "stock": 19,
+        "ratings": 5,
+        "ratingsCount": 4355,
+        "img": "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/2cee64414e1f4f31baf1ae270099d950_9366/Lunar_New_Year_Ultraboost_DNA_Shoes_Black_GZ6074_01_standard.jpg",
+        "shipping": 14,
+        "quantity": 0
+      },
+      {
+        "id": 3,
+        "category": "Men's Sneaker",
+        "name": "SUPERNOVA SHOES",
+        "seller": "Addidas",
+        "price": 245,
+        "stock": 20,
+        "ratings": 4,
+        "ratingsCount": 3972,
+        "img": "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/69cbc73d0cb846889f89acbb011e68cb_9366/Supernova_Shoes_Black_S42722_01_standard.jpg",
+        "shipping": 1,
+        "quantity": 0
+      },
+      {
+        "id": 4,
+        "category": "Men's Sneaker",
+        "name": "LITE RACER SHOES",
+        "seller": "Addidas",
+        "price": 229,
+        "stock": 10,
+        "ratings": 5,
+        "ratingsCount": 1764,
+        "img": "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/96a5f085fedf4e678095abad01056711_9366/Lite_Racer_Adapt_3.0_Shoes_Black_FX8802_01_standard.jpg",
+        "shipping": 32,
+        "quantity": 0
+      },
+      {
+        "id": 5,
+        "category": "Men's Sneaker",
+        "name": "4DFWD SHOES",
+        "seller": "Addidas",
+        "price": 287,
+        "stock": 11,
+        "ratings": 4,
+        "ratingsCount": 799,
+        "img": "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/8c5d1994dfd343e28567ad4400dd351d_9366/4DFWD_Shoes_Black_Q46447_01_standard.jpg",
+        "shipping": 49,
+        "quantity": 0
+      },
+      {
+        "id": 6,
+        "category": "Men's Sneaker",
+        "name": "KAPTIR 2.0 SHOES",
+        "seller": "Addidas",
+        "price": 138,
+        "stock": 19,
+        "ratings": 3,
+        "ratingsCount": 4372,
+        "img": "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/0846e90b15144861b33dacf500e3cfd1_9366/Kaptir_2.0_Shoes_White_H00276_01_standard.jpg",
+        "shipping": 19,
+        "quantity": 0
+      },
+    ]
+  }
+
+
   constructor(private route: ActivatedRoute, private router: Router) { }
   // constructor(private router: Router) { }
 
@@ -47,13 +131,30 @@ export class ProductEditComponent implements OnInit {
   }
 
   ngOnInit(): void {
+
     this.id = this.route.snapshot.paramMap.get('id');
     this.products = localStorage.getItem('products');
     this.allProducts = JSON.parse(this.products)
     this.allList = this.allProducts?.list;
 
     this.filterProduct = this.allList?.find((product: any) => product.id == this.id)
-    // console.log(this.filterProduct);
+    if (!this.filterProduct) {
+      const confirm = window.confirm('Do you want to Save data to LocalStorage .....')
+      if (!confirm) {
+        this.router.navigate(['']);
+        return
+      }
+      localStorage.setItem('products', JSON.stringify(this.product));
+      alert('Data save to Localstroage');
+
+      this.id = this.route.snapshot.paramMap.get('id');
+      this.products = localStorage.getItem('products');
+      this.allProducts = JSON.parse(this.products)
+      this.allList = this.allProducts?.list;
+
+      this.filterProduct = this.allList?.find((product: any) => product.id == this.id)
+
+    }
 
 
   }
